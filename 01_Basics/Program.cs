3391483e7af928433fab01bdca30f0608e94eaf2
BleytMC <150685954+BleytMC@users.noreply.github.com>
2025-11-11 19:13:45 +0200

    Add project files.

---
+++ 01_Basics/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Enter A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+for(int i = a; i <= b; i++)
+{
+    for(int j = 0; j < i; j++) Console.Write(i + " ");
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+Console.Write("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine()), c = 0;
+while(n != 0)
+{
+    c = (c * 10) + (n % 10);
+    n /= 10;
+}
+Console.WriteLine(c);
