122759baebc02702d0640a65218157a0d67c33e1
BleytMC <150685954+BleytMC@users.noreply.github.com>
2025-11-20 16:54:38 +0200

    4

---
+++ 10_PlaneHomework/Plane.cs
@@ -0,0 +1,46 @@
+﻿namespace _10_PlaneHomework
+{
+    partial class Plane
+    {
+        private string name;
+        private int year;
+        private int length;
+        private int wingspan;
+        private double price;
+
+        public Plane()
+        {
+            name = "No name";
+            year = 0;
+            length = 0;
+            wingspan = 0;
+            price = 0;
+        }
+
+        public Plane(string name, int year, int length, int wingspan, double price)
+        {
+            this.name = name;
+            this.year = year;
+            this.length = length;
+            this.wingspan = wingspan;
+            this.price = price;
+        }
+
+        public string GetName() { return name; }
+        public int GetYear() { return year; }
+        public int GetLength() { return length; }
+        public int GetWingspan() { return wingspan; }
+        public double GetPrice() { return price; }
+
+        public void SetName(string name) { this.name = name; }
+        public void SetYear(int year) { this.year = year; }
+        public void SetLength(int length) {this.length = length; }
+        public void SetWingspan(int wingspan) { this.wingspan = wingspan;}
+        public void SetPrice(double price) { this.price = price; }
+
+        public override string ToString()
+        {
+            return $"Name: {name}\n\tYear: {year}\n\tLength: {length}\n\tWingspan: {wingspan}\n\tPrice: {price}$";
+        }
+    }
+}
---
+++ 10_PlaneHomework/Program.cs
@@ -0,0 +1,11 @@
+﻿using _10_PlaneHomework;
+
+Plane[] planes = [
+    new Plane("Boeing 737", 1998, 39, 35, 82000000.0),
+    new Plane("Airbus A320", 2005, 37, 34, 98000000.0),
+    new Plane("Antonov An-225", 1988, 84, 88, 300000000.0),
+    new Plane("Embraer E190", 2010, 36, 29, 52000000.0),
+    new Plane("Boeing 787 Dreamliner", 2015, 63, 60, 250000000.0)
+];
+
+foreach (Plane plane in planes) Console.WriteLine(plane);
