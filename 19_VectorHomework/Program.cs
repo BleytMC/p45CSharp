ce22a3b6f593beae119df14de1a34aef6e0251c0
BleytMC <150685954+BleytMC@users.noreply.github.com>
2025-12-01 21:49:12 +0200

    9

---
+++ 19_VectorHomework/Program.cs
@@ -0,0 +1,93 @@
+﻿using _19_VectorHomework;
+
+Vector vec = new Vector();
+bool running = true;
+
+// Меню чат зробив
+
+while (running)
+{
+    Console.WriteLine("\n===== MENU =====");
+    Console.WriteLine("1. PushBack");
+    Console.WriteLine("2. PopBack");
+    Console.WriteLine("3. Insert");
+    Console.WriteLine("4. Erase");
+    Console.WriteLine("5. IsEmpty");
+    Console.WriteLine("6. At");
+    Console.WriteLine("7. Reverse");
+    Console.WriteLine("8. Clear");
+    Console.WriteLine("9. Print vector");
+    Console.WriteLine("0. Exit");
+    Console.Write("Select option: ");
+
+    string option = Console.ReadLine();
+    Console.WriteLine();
+
+    try
+    {
+        switch (option)
+        {
+            case "1":
+                Console.Write("Enter value: ");
+                int val = int.Parse(Console.ReadLine());
+                vec.PushBack(val);
+                break;
+
+            case "2":
+                Console.WriteLine("Removed: " + vec.PopBack());
+                break;
+
+            case "3":
+                Console.Write("Enter index: ");
+                int insIndex = int.Parse(Console.ReadLine());
+                Console.Write("Enter value: ");
+                int insVal = int.Parse(Console.ReadLine());
+                vec.Insert(insIndex, insVal);
+                break;
+
+            case "4":
+                Console.Write("Enter index: ");
+                int eraseIndex = int.Parse(Console.ReadLine());
+                vec.Erase(eraseIndex);
+                break;
+
+            case "5":
+                Console.WriteLine("Empty? " + vec.IsEmpty());
+                break;
+
+            case "6":
+                Console.Write("Enter index: ");
+                int atIndex = int.Parse(Console.ReadLine());
+                Console.WriteLine("Value: " + vec.At(atIndex));
+                break;
+
+            case "7":
+                vec.Reverse();
+                Console.WriteLine("Reversed!");
+                break;
+
+            case "8":
+                vec.Clear();
+                Console.WriteLine("Vector cleared!");
+                break;
+
+            case "9":
+                Console.WriteLine("Vector: " + vec.ToString());
+                break;
+
+            case "0":
+                running = false;
+                break;
+
+            default:
+                Console.WriteLine("Unknown option!");
+                break;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error: " + ex.Message);
+    }
+}
+
+Console.WriteLine("Goodbye!");
---
+++ 19_VectorHomework/Vector.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_VectorHomework
+{
+    class Vector
+    {
+        int[] mass;
+        int count;
+
+        public int Count { get { return count; } }
+
+        public Vector()
+        {
+            count = 0;
+            mass = new int[count];
+        }
+
+        public Vector(int[] mass)
+        {
+            count = mass.Length;
+            this.mass = mass;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", mass);
+        }
+
+        public void PushBack(int value)
+        {
+            int[] arr = new int[count + 1];
+            for (int i = 0; i < count; i++) arr[i] = mass[i];
+            arr[count] = value;
+            mass = arr;
+            count++;
+        }
+
+        public int PopBack()
+        {
+            int [] arr = new int[count - 1];
+            for (int i = 0; i < count - 1; i++) arr[i] = mass[i];
+            int res = mass[count - 1];
+            mass = arr;
+            count--;
+            return res;
+        }
+
+        public void Insert(int index, int value)
+        {
+            if(index > count || index < 0) throw new IndexOutOfRangeException();
+            int[] arr = new int[count + 1];
+            for(int i = 0; i < count + 1; i++)
+            {
+                if (i < index) arr[i] = mass[i];
+                else if (i > index) arr[i] = mass[i - 1];
+                else arr[i] = value;
+            }
+            mass = arr;
+            count++;
+        }
+
+        public void Erase(int index)
+        {
+            if(index >= count || index < 0) throw new IndexOutOfRangeException();
+            int[] arr = new int[count - 1];
+            for(int i = 0; i < count - 1; i++)
+            {
+                if (i < index) arr[i] = mass[i];
+                else arr[i] = mass[i + 1];
+            }
+            mass = arr;
+            count--;
+        }
+
+        public bool IsEmpty()
+        {
+            return count == 0;
+        }
+
+        public int At(int index)
+        {
+            if (index >= count || index < 0) throw new IndexOutOfRangeException();
+            return mass[index];
+        }
+
+        public void Reverse()
+        {
+            int[] arr = new int[count];
+            for (int i = 0; i < count; i++) arr[i] = mass[count - i - 1];
+            mass = arr;
+        }
+
+        public void Clear()
+        {
+            mass = new int[0];
+            count = 0;
+        }
+
+        public int this[int index]
+        {
+            get { return mass[index]; }
+        }
+    }
+}
