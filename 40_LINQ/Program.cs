087a9231bedff21b555ceaa00f4e834757029b66
BleytMC <150685954+BleytMC@users.noreply.github.com>
2026-01-07 19:48:19 +0200

    18

---
+++ 40_LINQ/Program.cs
@@ -0,0 +1,81 @@
+﻿//int[] arrayInt = { 5, 34, 67, 12, 94, 42 };
+
+// Синтаксис запиту
+
+// from, in, select
+
+//IEnumerable<int> query = from i in arrayInt select i;
+
+//foreach (int item in query)
+//{
+//    Console.Write($"{item} ");
+//}
+
+
+// where
+
+
+//bool isSimple(int a)
+//{
+//    for (int i = 2; i < a; i++) if (a % i == 0) return false;
+//    return true;
+//}
+
+//int[] arrayInt = { 5, 34, 67, 12, 94, 42 };
+
+//IEnumerable<int> query = from i in arrayInt where isSimple(i) select i;
+
+//foreach (int i in query) Console.Write($"{i} ");
+
+
+// orderby, ascending, descending
+
+//int[] arrayInt = { 5, 34, 67, 12, 94, 42 };
+
+//var query = from i in arrayInt where i % 2 == 0 orderby i ascending select i;
+
+//foreach (int i in query) Console.Write($"{i} ");
+
+
+using System.Collections;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+//string[] students = new string[20]
+//        {
+//            "Іваненко", "Петренко", "Сидоренко", "Коваленко", "Бондаренко",
+//            "Ткаченко", "Кравченко", "Олійник", "Шевченко", "Поліщук",
+//            "Лисенко", "Кузнецов", "Марченко", "Козак", "Мазур",
+//            "Мороз", "Кушнір", "Павленко", "Савченко", "Клименко"
+//        };
+
+
+//IEnumerable<IGrouping<char, string>> query = from s in students group s by s[0] into res where res.Count() > 2 select res;
+
+//foreach(var group in query)
+//{
+//    Console.WriteLine(group.Key);
+//    foreach(string s in group) Console.WriteLine(s);
+//}
+
+//int[] arrayInt = { 5, 34, 67, 12, 94, 42 };
+
+//IEnumerable<int> query = arrayInt.Where(i => i % 2 == 0).OrderBy(i => i).Select(i => i);
+
+//foreach (int i in query) Console.WriteLine(i);
+
+string[] students = new string[20]
+        {
+            "Іваненко", "Петренко", "Сидоренко", "Коваленко", "Бондаренко",
+            "Ткаченко", "Кравченко", "Олійник", "Шевченко", "Поліщук",
+            "Лисенко", "Кузнецов", "Марченко", "Козак", "Мазур",
+            "Мороз", "Кушнір", "Павленко", "Савченко", "Клименко"
+        };
+
+var query = students.GroupBy(s => s[0]).Where(g => g.Count() > 1);
+
+foreach (var group in query)
+{
+    Console.WriteLine(group.Key);
+    foreach (string s in group) Console.WriteLine(s);
+}
---
+++ 41_LINQPractice/Program.cs
@@ -0,0 +1,99 @@
+﻿string[] cars = {
+    "Nissan", "Chevrolet", "Tesla", "Cadillac", "Hummer", "Alfa Romeo", "Land Rover",
+    "Chrysler", "Bentley", "Renault", "Suzuki", "Honda", "Dodge", "BMW", "Ferrari", "Audi",
+    "Hyundai", "Skoda", "KIA", "Bugatti","Bentley", "SEAT", "Maserati", "Ford", "Buick", "Lexus",
+    "Mercedes", "Toyota", "Lincoln", "Volvo", "Aston Martin", "Fiat", "Subaru", "Maybach",
+    "Infiniti", "Volkswagen","Opel", "Citroen", "Mazda", "Jaguar", "Peugeot", "Dacia", "Jeep", "Lamborghini"
+};
+
+// Select
+
+// 1
+
+//var query = from car in cars select car;
+
+//foreach(var item in query) Console.WriteLine(item);
+
+// 2
+
+//var query = from car in cars select car.Length;
+
+//foreach (var item in query) Console.WriteLine(item);
+
+// 3
+
+//var query = from car in cars select car + "!";
+
+//foreach (var item in query) Console.WriteLine(item);
+
+
+// Where
+
+// 1
+
+//var query = from car in cars where car[car.Length - 1] == 'a' select car;
+
+//foreach (var item in query) Console.WriteLine(item);
+
+// 3
+
+//var query = from car in cars where car.Length == 5 select car;
+
+//foreach (var item in query) Console.WriteLine(item);
+
+// 6
+
+//var query = from car in cars where car.Contains('s') select car.ToUpper();
+
+//foreach (var item in query) Console.WriteLine(item);
+
+
+// orderby
+
+// 1
+
+//var query = from car in cars orderby car ascending select car;
+
+//var query = from car in cars orderby car descending select car;
+
+//foreach (var item in query) Console.WriteLine(item);
+
+// 2
+
+//var query = from car in cars orderby car.Length ascending select car;
+
+//foreach (var item in query) Console.WriteLine(item);
+
+// 3
+
+//var query = from car in cars where car.Length > 4 orderby car ascending select car;
+
+//var query = from car in cars where car.Length > 4 orderby car descending select car;
+
+//foreach (var item in query) Console.WriteLine(item);
+
+
+// group by
+
+// 3
+
+//var query = from car in cars group car by car.Length;
+
+//foreach (var group in query)
+//{
+//    Console.WriteLine(group.Key);
+//    foreach(string car in group) Console.WriteLine(car);
+//}
+
+
+// into
+
+// 1
+
+//var query = from car in cars group car by car[0] into res where res.Count() > 1 select res;
+
+//foreach (var group in query)
+//{
+//    Console.WriteLine(group.Key);
+//    foreach(string car in group) Console.WriteLine(car);
+//}
